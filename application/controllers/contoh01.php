e073f9587955659739e2e4d8b9186ae1cdb999b7
Zibrianwaskito <zibrian.waskito@gmail.com>
2023-09-27 13:40:35 +0700

    menambahkan contoh01.php

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama saya zibrian
+            saya tingga di daerah ciputat
+            olah raga yang saya sukai adalah 
+            Bulutangkis";
+    }
+}
