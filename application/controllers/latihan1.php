24b2746206ae9445d44d8888853a2ca0583c9069
Zibrianwaskito <zibrian.waskito@gmail.com>
2023-10-10 15:54:28 +0700

    mengerjakan tugas pertemuan 3

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,19 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat datang... selamat belajar Web Programming";
+        //this->load->view('view-latihan1');
+    }
+    
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_latihan1');
+        $data['nilai 1'] = $n1;
+        $data['nilai 2'] = $n2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+
+        $this->load->view('view-latihan1', $data);
+    }
+}
